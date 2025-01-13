7c29b54306964ed77a4577eabdd2c4ca252102a2
marcin.kwiatkowski <marcinkwiatkowski173@gmail.com>
2025-01-13 20:54:46 +0100

    Fix test EmptyEventItemsTest.

--- src/application/db/event.cpp
+++ src/application/db/event.cpp
@@ -5,7 +5,7 @@ namespace db
 {
 
   Event::Event(const int id, EventItems &&eventItems)
-      : m_id(id), m_events(eventItems)
+      : m_id(id), m_events(std::move(eventItems))
   {
   }
 
@@ -22,9 +22,16 @@ namespace db
   const std::string Event::findByKey(const std::string &key) const
   {
 
-    return std::ranges::find_if(m_events, [&key](const auto &item)
-                                { return item.first == key; })
-        ->second;
+    std::string result{""};
+    auto pair = std::ranges::find_if(m_events, [&key](const auto &item)
+                                     { return item.first == key; });
+
+    if (pair != m_events.end())
+    {
+      result = pair->second;
+    }
+
+    return result;
   }
 
   Event::EventItemsIterator Event::findInEvent(const std::string &search)
