5947660216fda63e5208a4865b9bdff9f4864bd0
marcin.kwiatkowski <marcinkwiatkowski173@gmail.com>
2025-05-13 20:23:46 +0200

    Update cmakefiles to check if scripts cleanCppExtensions, sanitizers exists.

--- tests/XmlParserTest.cpp
+++ tests/XmlParserTest.cpp
@@ -1,37 +1,37 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <sstream>
-#include "src/application/xml/XmlParser.hpp"
+#include "src/application/xml/xmlParser.hpp"
 #include "src/application/db/LogEvent.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <sstream>
 
 class MockObserver : public parser::IDataParserObserver
 {
-public:
-  MOCK_METHOD(void, NewEventFound, (db::LogEvent && event), (override));
-  MOCK_METHOD(void, ProgressUpdated, (), (override));
+  public:
+    MOCK_METHOD(void, NewEventFound, (db::LogEvent && event), (override));
+    MOCK_METHOD(void, ProgressUpdated, (), (override));
 };
 
 class XmlParserTest : public ::testing::Test
 {
-protected:
-  MockObserver mockObserver;
-  parser::XmlParser parser;
+  protected:
+    MockObserver mockObserver;
+    parser::XmlParser parser;
 
-  void SetUp() override
-  {
-    // Set up code if needed
-    parser.RegisterObserver(&mockObserver);
-  }
+    void SetUp() override
+    {
+        // Set up code if needed
+        parser.RegisterObserver(&mockObserver);
+    }
 
-  void TearDown() override
-  {
-    // Clean up code if needed
-  }
+    void TearDown() override
+    {
+        // Clean up code if needed
+    }
 };
 
 TEST_F(XmlParserTest, ParseValidXml)
 {
-  std::string xmlData = R"(
+    std::string xmlData = R"(
         <events>
             <event>
                 <timestamp>2025-01-14T15:20:55</timestamp>
@@ -41,22 +41,24 @@ TEST_F(XmlParserTest, ParseValidXml)
         </events>
     )";
 
-  std::istringstream input(xmlData);
+    std::istringstream input(xmlData);
 
-  EXPECT_CALL(mockObserver, NewEventFound(testing::_))
-      .Times(1)
-      .WillOnce([](const db::LogEvent &event)
-                {
-            EXPECT_EQ(event.findByKey("timestamp"), "2025-01-14T15:20:55");
-            EXPECT_EQ(event.findByKey("type"), "INFO");
-            EXPECT_EQ(event.findByKey("info"), "Test event"); });
+    EXPECT_CALL(mockObserver, NewEventFound(testing::_))
+        .Times(1)
+        .WillOnce(
+            [](const db::LogEvent& event)
+            {
+                EXPECT_EQ(event.findByKey("timestamp"), "2025-01-14T15:20:55");
+                EXPECT_EQ(event.findByKey("type"), "INFO");
+                EXPECT_EQ(event.findByKey("info"), "Test event");
+            });
 
-  parser.ParseData(input);
+    parser.ParseData(input);
 }
 
 TEST_F(XmlParserTest, ParseInvalidXml)
 {
-  std::string xmlData = R"(
+    std::string xmlData = R"(
         <events>
             <event>
                 <timestamp>2025-01-14T15:20:55</timestamp>
@@ -65,9 +67,9 @@ TEST_F(XmlParserTest, ParseInvalidXml)
             <!-- Missing closing tags for event and events -->
     )";
 
-  std::istringstream input(xmlData);
+    std::istringstream input(xmlData);
 
-  EXPECT_CALL(mockObserver, NewEventFound(testing::_)).Times(0);
+    EXPECT_CALL(mockObserver, NewEventFound(testing::_)).Times(0);
 
-  parser.ParseData(input);
+    parser.ParseData(input);
 }
