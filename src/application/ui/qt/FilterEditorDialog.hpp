1e218509a017180eea34f3f9f32d7d115ea521bc
marcin.kwiatkowski <marcinkwiatkowski173@gmail.com>
2025-12-09 12:58:58 +0100

    Add complex filtering.

--- src/application/ui/qt/FilterEditorDialog.hpp
+++ src/application/ui/qt/FilterEditorDialog.hpp
@@ -9,8 +9,12 @@ class QCheckBox;
 class QComboBox;
 class QDialogButtonBox;
 class QLineEdit;
+class QListWidget;
+class QListWidgetItem;
+class QPushButton;
 class QSpinBox;
 class QStackedWidget;
+class QVBoxLayout;
 
 namespace ui::qt
 {
@@ -28,6 +32,10 @@ class FilterEditorDialog : public QDialog
   private slots:
     void OnFilterTypeChanged(int index);
     void HandleAccept();
+    void OnAddCondition();
+    void OnRemoveCondition();
+    void OnConditionSelectionChanged();
+    void OnConditionItemDoubleClicked(QListWidgetItem* item);
 
   private:
     void BuildUi();
@@ -35,9 +43,16 @@ class FilterEditorDialog : public QDialog
     QWidget* BuildParameterPage();
     void PopulateColumns();
     bool ValidateAndPersist();
+    void UpdateConditionList();
+    void LoadConditionIntoEditor(int conditionIndex);
+    void SaveConditionFromEditor(int conditionIndex);
+    filters::FilterCondition CreateConditionFromEditor();
+    void ClearConditionEditor();
 
     filters::FilterPtr m_filter;
+    std::vector<filters::FilterCondition> m_conditions;
 
+    // Main UI elements
     QComboBox* m_filterTypeCombo {nullptr};
     QStackedWidget* m_filterPages {nullptr};
     QComboBox* m_columnCombo {nullptr};
@@ -48,6 +63,20 @@ class FilterEditorDialog : public QDialog
     QCheckBox* m_caseSensitiveCheck {nullptr};
     QCheckBox* m_invertedCheck {nullptr};
     QDialogButtonBox* m_buttonBox {nullptr};
+
+    // Complex filter UI elements
+    QListWidget* m_conditionsList {nullptr};
+    QPushButton* m_addConditionBtn {nullptr};
+    QPushButton* m_removeConditionBtn {nullptr};
+    QWidget* m_conditionEditorWidget {nullptr};
+    QComboBox* m_conditionTypeCombo {nullptr};
+    QComboBox* m_conditionColumnCombo {nullptr};
+    QLineEdit* m_conditionParameterKeyEdit {nullptr};
+    QSpinBox* m_conditionDepthSpin {nullptr};
+    QLineEdit* m_conditionPatternEdit {nullptr};
+    QCheckBox* m_conditionCaseSensitiveCheck {nullptr};
+
+    int m_currentEditingCondition {-1};
 };
 
 } // namespace ui::qt
