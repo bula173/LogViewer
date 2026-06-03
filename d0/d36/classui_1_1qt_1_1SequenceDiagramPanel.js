de4f0e02baf5765b460e80d2e33b0c1d28dc16c2
bula173 <bula173@users.noreply.github.com>
2026-06-03 05:18:14 +0000

    docs: update Doxygen for main 58627351fa555c3701f18130b66146986e9d18ba

--- d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.js
+++ d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.js
@@ -2,6 +2,7 @@ var classui_1_1qt_1_1SequenceDiagramPanel =
 [
     [ "SequenceDiagramPanel", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#aaa04f7d6a5bdbb5563df46ac032e1b7b", null ],
     [ "BuildLayout", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a7df3ff51bb1dbfaecca6556d8ec0d4ba", null ],
+    [ "OnDiscoveryFinished", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a9e93f2214309a662012e08a50690ac84", null ],
     [ "OnLimitChanged", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a9dcc87a89a1ef7724ad3b99a44dc2f93", null ],
     [ "OnSceneClicked", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a954c9e838cb86b3c1d7ceb9f633662aa", null ],
     [ "Refresh", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#ae2ebbd0bb141e5e210198b389b0b548a", null ],
@@ -10,8 +11,9 @@ var classui_1_1qt_1_1SequenceDiagramPanel =
     [ "m_eventsView", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a9947adf01f36e9ec951b613831f07c59", null ],
     [ "m_limitSpin", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#ac2ed206ad79a3089ecdd719873a520db", null ],
     [ "m_pattern", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a0882343f46421e18ac47ca38bd2966dc", null ],
-    [ "m_refreshing", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a376c63fe0e88d0fad2012b1fe392aee5", null ],
+    [ "m_refreshBtn", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a646ed32344c1df47872f96504fe7b95f", null ],
     [ "m_scene", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a3a5528a847c316b6d752554c82a7b071", null ],
     [ "m_statusLabel", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#a648320590e15574c33e2b1860630c7f0", null ],
-    [ "m_view", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#acb3d7d37e9477cbe2d501ea76210f226", null ]
+    [ "m_view", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#acb3d7d37e9477cbe2d501ea76210f226", null ],
+    [ "m_watcher", "d0/d36/classui_1_1qt_1_1SequenceDiagramPanel.html#aa385df5537fee5940af82cc4a89d8545", null ]
 ];
--- df/d2a/classArrowItem.js
+++ df/d2a/classArrowItem.js
@@ -2,5 +2,6 @@ var classArrowItem =
 [
     [ "ArrowItem", "df/d2a/classArrowItem.html#a99e64aa182ef4b20a834ddf7b77ca183", null ],
     [ "eventIndex", "df/d2a/classArrowItem.html#a803418b8d5087eda8626a3cd0c03707d", null ],
+    [ "shape", "df/d2a/classArrowItem.html#a8ed2b6e002e0ba6ed6ef4ac15b29c22f", null ],
     [ "m_eventIdx", "df/d2a/classArrowItem.html#a546ef013fb05f37c2d0f9e3b9abe4e7d", null ]
 ];
